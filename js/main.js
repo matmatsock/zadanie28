9f4e5c5ba2131a2eb4be96acf5ce5355b8c4d48f
mateusz socko <dataochi@gmail.com>
2021-04-18 18:30:01 +0200

    zadanie28

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let core = 12;
+if (core>10){
+    console.log('liczba jest większa od dziesięciu');
+} else if (core<10){
+    console.log('liczba jest mniejsza od dziesięciu');
+} else {
+    console.log('liczba jest równa 10'); 
+} 
